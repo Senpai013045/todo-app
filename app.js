2d15a821f879dfa56d57aa2421d1326e4532a761
Senpai013045 <59314575+Senpai013045@users.noreply.github.com>
2020-07-05 08:09:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ var todoController = require("./controllers/todoController");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 todoController(app);
-app.listen(8080);
+app.listen(process.env.PORT||8080);
 console.log("now listening to port 8080");
 //my server file
